d898443c033d4d9d9914d6c915af9f7ab1ce8f79
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2024-03-13 14:49:16 -0300

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,22 @@
+# Importação de bibliotecas
+import pandas as pd
+import geopandas as gpd
+import plotly.express as px
+import streamlit as st
+import plotly.graph_objects as go
+import requests
+import folium
+from streamlit_folium import st_folium, folium_static
+
+# Configurações da página
+st.set_page_config(
+    page_title="Alertas INMET - RS",
+    page_icon=":warning:",
+    #layout="wide",
+    initial_sidebar_state='collapsed'
+)
+col1, col2, col3 = st.columns([1,4,1])
+
+col1.image('https://github.com/andrejarenkow/csv/blob/master/logo_cevs%20(2).png?raw=true', width=200)
+col2.title('Alertas INMET - RS')
+col3.image('https://github.com/andrejarenkow/csv/blob/master/logo_estado%20(3)%20(1).png?raw=true', width=300)
