b2b3502021cdce7091cb0e6f1bd23f7df27bcc6e
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2024-03-13 14:54:46 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,6 +17,6 @@ st.set_page_config(
 )
 col1, col2, col3 = st.columns([1,4,1])
 
-col1.image('https://github.com/andrejarenkow/csv/blob/master/logo_cevs%20(2).png?raw=true', width=200)
+col1.image('https://github.com/andrejarenkow/csv/blob/master/logo_cevs%20(2).png?raw=true', width=100)
 col2.title('Alertas INMET - RS')
-col3.image('https://github.com/andrejarenkow/csv/blob/master/logo_estado%20(3)%20(1).png?raw=true', width=300)
+col3.image('https://github.com/andrejarenkow/csv/blob/master/logo_estado%20(3)%20(1).png?raw=true', width=150)
