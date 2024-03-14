34ea362611d1d877882ae0abf19b5400cd32b7fe
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2024-03-14 15:47:50 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -175,6 +175,6 @@ Fonte: https://alertas2.inmet.gov.br/
         st.markdown(html_button, unsafe_allow_html=True)
 
     except:
-        st.write('Clique em algum polígono e aguarde para obter informações sobre alertas.')
+        st.write('Clique na área de interesse e aguarde para obter informações sobre alertas.')
     
     
